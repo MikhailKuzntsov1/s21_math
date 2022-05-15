a63ac3d6c4ce99011915fd4379759a3e681417aa
Tartica Rosalba <tarticar@vo-a2.msk.21-school.ru>
2022-05-15 09:56:56 +0300

    some tests added + functions fixes

--- main.c
+++ main.c
@@ -9,7 +9,8 @@ int main(void) {
 void run_testcase(Suite *testcase) {
     static int counter_testcase = 1;
 
-    if (counter_testcase > 1) putchar('\n');
+    if (counter_testcase > 1)
+        putchar('\n');
     printf("%s%d%s", "CURRENT TEST: ", counter_testcase, "\n");
     counter_testcase++;
 
@@ -22,14 +23,18 @@ void run_testcase(Suite *testcase) {
 }
 
 void run_tests(void) {
-    Suite *list_cases[] = {// suite_s21_abs(),  // suite_s21_acos(), suite_s21_asin(), suite_s21_atan(),
-                           //                   suite_s21_ceil(),
-                           suite_s21_cos(),  // suite_s21_exp(), suite_s21_fabs(),
-                                             //  suite_s21_floor(), suite_s21_fmod(), suite_s21_log(),  suite_s21_pow(),
-                           suite_s21_sin(),  //   suite_s21_sqrt(),
-                           suite_s21_tan(), NULL};
+    Suite *list_cases[] = {
+        suite_s21_abs(),  // suite_s21_acos(),
+        // suite_s21_asin(), suite_s21_atan(),
+        suite_s21_ceil(), suite_s21_cos(),
+        suite_s21_fabs(),  // suite_s21_exp(),
+                           //   suite_s21_floor(), suite_s21_fmod(),
+                           //   suite_s21_log(),  suite_s21_pow(),
+        suite_s21_sin(),   //   suite_s21_sqrt(),
+        suite_s21_tan(), NULL};
 
-    for (Suite **current_testcase = list_cases; *current_testcase != NULL; current_testcase++) {
+    for (Suite **current_testcase = list_cases; *current_testcase != NULL;
+         current_testcase++) {
         run_testcase(*current_testcase);
     }
 }
@@ -48,4 +53,9 @@ int RandomInteger(int low, int high) {
     d = (double)rand() / ((double)RAND_MAX + 1);
     k = (int)(d * (high - low + 1));
     return (low + k);
+}
+
+double fRand(double fMin, double fMax) {
+    double f = (double)rand() / RAND_MAX;
+    return fMin + f * (fMax - fMin);
 }
