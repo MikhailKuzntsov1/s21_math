574f03dae684b0c4b928fbdec1cd8162418a983b
Posidonius Kathyrn <posidoni@su-g5.msk.21-school.ru>
2022-05-15 20:40:11 +0300

    Developed atan

--- main.c
+++ main.c
@@ -22,14 +22,18 @@ void run_testcase(Suite *testcase) {
     srunner_free(sr);
 }
 void run_tests(void) {
-    Suite *list_cases[] = {suite_s21_abs(),  // suite_s21_acos(),
-                                             // suite_s21_asin(), suite_s21_atan(),
-                           suite_s21_ceil(), suite_s21_cos(),
-                           suite_s21_fabs(),   // suite_s21_exp(),
-                           suite_s21_floor(),  // suite_s21_fmod(),
-                           //   suite_s21_log(),  suite_s21_pow(),
-                           suite_s21_sin(),  //   suite_s21_sqrt(),
-                           suite_s21_tan(), NULL};
+    Suite *list_cases[] = {
+        // suite_s21_abs(),  // suite_s21_acos(),
+        //                      // suite_s21_asin(), suite_s21_atan(),
+        //    suite_s21_ceil(),
+        //    suite_s21_cos(),
+        //    suite_s21_fabs(),   // suite_s21_exp(),
+        //    suite_s21_floor(),  // suite_s21_fmod(),
+        //    //   suite_s21_log(),  suite_s21_pow(),
+        //    suite_s21_sin(),  //   suite_s21_sqrt(),
+        //    suite_s21_tan(),
+        suite_s21_atan(),
+        NULL};
 
     for (Suite **current_testcase = list_cases; *current_testcase != NULL;
          current_testcase++) {
